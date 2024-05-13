80f6a8e90596dc25f3257db32980b259cb4b68a6
limjh0222 <97434772+limjh0222@users.noreply.github.com>
2024-05-13 16:55:58 +0900

    Add CameraController
    - 카메라가 캐릭터 따라가도록 구현

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera _camera;
+    [SerializeField] private GameObject _player;
+    private Vector3 _cameraPositionZ = new Vector3(0, 0, -10);
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = _player.transform.position + _cameraPositionZ;
+    }
+}
